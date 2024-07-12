875411f8b9981111a0146d294f25672c91f508c3
Mosazghi <fest121@hotmail.com>
2024-07-13 00:09:54 +0200

    add styles

---
+++ mobile/constants/tokens.ts
@@ -0,0 +1,17 @@
+export const colors = {
+    primary: "#2D2D2D",
+    textMuted: "#9CA3AF",
+    blur: "rgba(0, 0, 0, 0.5)",
+    background: "#F9FAFB",
+    text: "#1F2937",
+};
+export const fontSize = {
+    xs: 12,
+    sm: 16,
+    base: 20,
+    lg: 24,
+};
+
+export const screenPadding = {
+    horizontal: 24,
+};
