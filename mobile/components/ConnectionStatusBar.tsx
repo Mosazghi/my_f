dee1e47d5e6a29aac946d41519a601d57268e1aa
Mosazghi <fest121@hotmail.com>
2024-07-14 16:56:21 +0200

    refactor(mobile): rename to connectionbarstatus

---
+++ mobile/components/ConnectionStatusBar.tsx
@@ -0,0 +1,40 @@
+import { View } from "react-native";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import * as Animatable from "react-native-animatable";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { Link } from "expo-router";
+
+export function ConnectionStatusBar() {
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const isMqttConnected = useSelector((state: RootState) => state.mqtt.connected);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsVisible((prevState) => !prevState);
+        }, 1000);
+
+        console.info("Connection status bar", isMqttConnected);
+
+        if (!isMqttConnected) clearInterval(interval);
+        return () => clearInterval(interval);
+    }, [isMqttConnected]);
+
+    return (
+        <View>
+            <View>
+                <Animatable.View
+                    animation={isVisible ? "fadeIn" : "fadeOut"}
+                    duration={1000}
+                >
+                    <FontAwesome6
+                        name="tower-broadcast"
+                        size={19}
+                        color={isMqttConnected ? "green" : "orangered"}
+                    />
+                </Animatable.View>
+            </View>
+        </View>
+    );
+}
