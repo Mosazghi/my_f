fe8270ca8ccec17be0528572e7b0a16a81af3c7e
Mosazghi <fest121@hotmail.com>
2024-07-15 23:39:49 +0200

    refactor(mobile): rename variable

--- mobile/util/mqtt.ts
+++ mobile/util/mqtt.ts
@@ -1,16 +1,7 @@
 import init from "react_native_mqtt";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { store } from "@/redux/store";
+import Store from "@/redux/store";
 import { setConnected, addMessage, NewScanMessage } from "../redux/mqtt";
-
-enum MqttSate {
-    CONNECTED = "CONNECTED",
-    NOT_CONNECTED = "NOT_CONNECTED",
-}
-
-const HOST = "192.168.1.168";
-// const HOST = "10.0.0.7";
-const PORT = 8080;
 
 // Init the mqtt client
 init({
@@ -24,28 +15,19 @@ init({
 
 let client: Paho.MQTT.Client | null = null;
 
-let mqttState = MqttSate.NOT_CONNECTED;
-
-export function isMqttConnected(): boolean {
-    return mqttState === MqttSate.CONNECTED;
-}
-
 function onConnect(topic: string) {
     console.info("onConnect:: topic", topic);
     client?.subscribe(topic, { qos: 2 });
-    mqttState = MqttSate.CONNECTED;
-    store.dispatch(setConnected(true));
+    Store.dispatch(setConnected(true));
 }
 
 function onFailure(error: Error) {
-    mqttState = MqttSate.NOT_CONNECTED;
-    store.dispatch(setConnected(false));
-    console.error("MQTT :: connect error", error);
+    Store.dispatch(setConnected(false));
+    console.info("MQTT :: connect error", error);
 }
 
 function onConnectionLost(responseObject: Paho.MQTT.MQTTError) {
-    mqttState = MqttSate.NOT_CONNECTED;
-    store.dispatch(setConnected(false));
+    Store.dispatch(setConnected(false));
     if (responseObject.errorCode !== 0) {
         setTimeout(() => {
             console.log(`onConnectionLost:${responseObject.errorMessage}`);
@@ -62,52 +44,45 @@ function onMessageArrived(message: Paho.MQTT.Message) {
         payload = message.payloadString;
     }
 
-    store.dispatch(addMessage({ ...payload }));
+    Store.dispatch(addMessage({ ...payload }));
 }
 
 export const MqttUtils = {
-    connect: (clientId: string, topic: string) => {
-        client = new Paho.MQTT.Client(HOST, PORT, clientId);
+    connect: (host: string, port: number, callback: (error?: Error) => void) => {
+        const clientId = "mobile";
+        const topic = "scanResult";
+        client = new Paho.MQTT.Client(host, port, clientId);
 
         console.info("MQTT :: connecting...");
 
         client.onConnectionLost = onConnectionLost;
         client.onMessageArrived = onMessageArrived;
 
-        try {
-            client.connect({
-                onSuccess: () => {
-                    onConnect(topic);
-                },
-                onFailure,
-                timeout: 3,
-                reconnect: true,
-            });
-        } catch (error) {
-            onFailure(error as Error);
-        }
+        client.connect({
+            onSuccess: () => {
+                onConnect(topic);
+                callback(); // No error, connection successful
+            },
+            onFailure: (error: Error) => {
+                callback(error); // Pass the error to the callback
+            },
+            timeout: 3,
+            reconnect: true,
+        });
     },
 
     disconnect: () => {
-        try {
-            if (client) {
-                client.disconnect();
-                client = null;
-                console.error("MQTT :: disconnected");
-            }
-        } catch (error) {
-            onFailure(error as Error);
+        if (client) {
+            client.disconnect();
+            client = null;
+            console.info("MQTT :: disconnected");
         }
     },
 
     unsubscribe: (topic: string) => {
-        try {
-            if (client) {
-                client.unsubscribe(topic);
-                console.error("MQTT :: unsubscribed");
-            }
-        } catch (error) {
-            onFailure(error as Error);
+        if (client) {
+            client.unsubscribe(topic);
+            console.info("MQTT :: unsubscribed");
         }
     },
 };
